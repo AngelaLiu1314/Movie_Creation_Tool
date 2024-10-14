7633afafae934b8d9a44749bcf1bff8b3c0a2e1e
Ian Chang <personal.ianchang@gmail.com>
2024-10-14 15:49:38 -0400

    added apiMain.py using Fast API header

---
+++ api/apiMain.py
@@ -0,0 +1,14 @@
+from typing import Union
+from fastapi import FastAPI
+
+# Use bash command fastapi dev ./api/apiMain.py
+app = FastAPI()
+
+@app.get("/")
+def read_root():
+    return {"Hello": "World"}
+
+
+@app.get("/items/{item_id}")
+def read_item(item_id: int, q: Union[str, None] = None):
+    return {"item_id": item_id, "q": q}
