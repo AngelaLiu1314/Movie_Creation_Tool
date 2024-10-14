8250180b0e0bd77d0acca27dd6f21f4236cee1e9
Ian Chang <personal.ianchang@gmail.com>
2024-10-13 21:04:58 -0400

    Add updateEntry.py

---
+++ databaseFunctions/updateEntry.py
@@ -0,0 +1,23 @@
+import json
+import os
+from dotenv import load_dotenv
+import pymongo
+from bson.objectid import ObjectId
+import requests
+import openai
+import pandas as pd
+
+load_dotenv() 
+mongodb_uri = os.getenv('MONGODB_URI') #retrieve mongodb uri from .env file
+
+try:
+    client = pymongo.MongoClient(mongodb_uri) # this creates a client that can connect to our DB
+    db = client.get_database("movies") # this gets the database named 'Movies'
+    movieDetails = db.get_collection("movieDetails")
+
+    client.server_info() # forces client to connect to server
+    print("Connected successfully to the 'Movies' database!")
+
+except pymongo.errors.ConnectionFailure as e:
+    print(f"Could not connect to MongoDB: {e}")
+    exit(1)
