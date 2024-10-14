3d6066d7a50625ced50d8ef464828b03a03a4e3d
Ian Chang <personal.ianchang@gmail.com>
2024-10-14 09:20:12 -0400

    added 1000 more records to db

--- databaseFunctions/createEntry.py
+++ databaseFunctions/createEntry.py
@@ -155,7 +155,7 @@ def add_movie_details(imdbID, response, indexInDF): #defines what information we
 #     main()
 
 # Start adding movie details to the database. Max daily responses for OMDB API is 1000, so we need to use an indexing variable to avoid repetitive addition
-lastIndex = 850 # Please try to update it based on the printed lastIndex before closing out
+lastIndex = 1850 # Please try to update it based on the printed lastIndex before closing out
 dailyBatchSize = 1000
 
 for imdbID in mainDF.imdb_id[lastIndex:lastIndex + dailyBatchSize]:
