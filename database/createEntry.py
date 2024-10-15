2d0f89a01beeb7c681aac9d5c67816ba50a0fa1b
NYU <nyu@10-18-142-201.dynapool.wireless.nyu.edu>
2024-10-15 16:03:30 -0400

    postman interaction

--- database/createEntry.py
+++ database/createEntry.py
@@ -160,7 +160,7 @@ def add_movie_details(imdbID, response, indexInDF): #defines what information we
 
 # Start adding movie details to the database. Max daily responses for OMDB API is 1000, so we need to use an indexing variable to avoid repetitive addition
 lastIndex = 97033 # Please try to update it based on the printed lastIndex before closing out
-dailyBatchSize = 100000
+dailyBatchSize = 80000
 
 for imdbID in mainDF.imdb_id[lastIndex:lastIndex + dailyBatchSize]:
     response, indexInDF = get_omdb_response(imdbID)
