73a0b0f441b6e0f5be85327c95539ff85b459200
NYU <nyu@10-18-142-201.dynapool.wireless.nyu.edu>
2024-11-13 15:36:29 -0500

    started adding releaseYear for the retro? call

--- database/movieEmbeddingsUpdate.py
+++ database/movieEmbeddingsUpdate.py
@@ -14,6 +14,7 @@ import torch
 from torchvision import transforms, models
 from torchvision.models import ResNet18_Weights
 from transformers import BlipProcessor, BlipForConditionalGeneration
+import datetime
 
 load_dotenv() 
 mongodb_uri = os.getenv('Mongo_URI') #retrieve mongodb uri from .env file
@@ -37,29 +38,53 @@ def add_style(movie):
     print()
 
 def add_release_year(movie):
-    print()
+    release_date = movie.get("releaseDate")
+    if release_date:
+        try:
+            date_obj = datetime.datetime.strptime(release_date, "%Y-%m-%d")
+            release_year = str(date_obj.year)
+        except ValueError:
+            print(f"Incorrect date format for movie ID {movie['_id']}. Skipping.")
+            return
+    else:
+        print(f"No release date for movie ID {movie['_id']}. Skipping.")
+        return
+
+    try:
+        movieEmbeddings.update_one(
+            {"movie_id": movie["_id"]},
+            {"$set": {"releaseYear": release_year}}
+        )
+        print(f"release year for movie_id: {movie['_id']} added")
+        
+    except Exception as e:
+        print(f"Error updating document {movie['_id']} in MongoDB: {e}")
+        traceback.print_exc()
+    
     
 # Uncomment the update function you want to run
 
-start_after_id = "672e783692eedf81f91b1b6a"
+all_ids = [movie["_id"] for movie in movieDetails.find({}, {"_id": 1}).sort("_id", 1)]
+batch_size = 100000
+batch_index = 0
 
-query_filter = {"_id": {"$gte": ObjectId(start_after_id)}}
-batch_size = 300000
-cursor = movieDetails.find(query_filter).sort("_id", 1).limit(batch_size)
+for id in all_ids[batch_index:batch_index+batch_size]:
+    batch_index += 1
+    try:
+        # Retrieve the full movie document using the current id
+        movie = movieDetails.find_one({"_id": id})
+        
+        if movie:
+            # add_style(movie)
+            add_release_year(movie)
+            # print() # Comment this line out when you run any of the update functions
+        else:
+            print(f"Movie with ID {id} not found.")
+    except Exception as e:
+        print("Error in processing the movie")
+        continue
 
-while True:
-    batch = list(cursor)
-    if not batch:
-        break
-    for movie in batch:
-        try:
-            # update_documents_posterImage(movie)
-            # classify_style(movie)
-            # update_documents_trainingPrompt(movie)
-            print() # Comment this line out when you run any of the update functions
-        except Exception as e:
-            print("Error in processing the movie")
-            continue
+print(f"New batch index: {batch_index}")
     
 
 # Close connection once finished
